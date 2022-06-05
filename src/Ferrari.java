cfafc22a112a96f5cd7bc0ccca9dfdd6f156f3d2
Odhran <mahonyodhran@gmail.com>
2022-06-05 17:48:20 +0100

    updated

--- src/Ferrari.java
+++ src/Ferrari.java
@@ -1,3 +1,29 @@
 public class Ferrari extends Car {
-    
+    private int roadServiceMonths;
+
+    public Ferrari(int roadServiceMonths) {
+        super("Ferrari", "Supercar", 4, 3, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate){
+        // rate = dif in speed from cur speed
+        int newSpeed = getCurrentSpeed() + rate;
+        if(newSpeed == 0){
+            stop();
+        }else if (newSpeed > 0 && newSpeed <= 10) {
+            changeGear(1);
+        
+        }else if (newSpeed > 10 && newSpeed <= 20) {
+            changeGear(2);
+        
+        }else if (newSpeed > 20 && newSpeed <= 30) {
+            changeGear(3);
+        }else{
+            changeGear(4);
+        }
+        if (newSpeed > 0) {
+            changeSpeed(newSpeed, getCurrentDirection());
+        }
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,49 @@
 public class Vehicle {
+    private String name;
+    private String size;
+
+    private int currentSpeed;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+
+        this.currentSpeed = 0;
+        this.currentDirection = 0;
+    }
+
+    public void steer(int direction){
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): Steering at " + currentDirection + " degrees.");
+    }
+
+    public void move(int speed, int direction){
+        currentSpeed = speed;
+        currentDirection = direction;
+        System.out.println("Vehicle.move(): Moving at " + currentSpeed + " in direction " + currentDirection);
+    }
+
+    public void stop(){
+        this.currentSpeed = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    
     
 }
